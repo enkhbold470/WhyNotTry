7fceabc0edcdd171a04fc3e138cf2547d07d98b2
enkhbold470 <enkhbold470@gmail.com>
2024-10-09 14:08:48 -0700

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Inky Ganbold on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
